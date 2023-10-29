450108628186a6d6f27dcd7391fe000523eb3640
Luka Avramovic <avramoviclu@gmail.com>
2023-10-29 19:30:01 +0100

    Adds Note controller.

---
+++ app/Http/Controllers/Api/v1/NoteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NoteController extends Controller
+{
+    public function all()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function update(string $id)
+    {
+
+    }
+
+    public function delete(string $id)
+    {
+
+    }
+}
