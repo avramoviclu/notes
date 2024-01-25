d531b9ceb525f08638b9b7a4f2deb5fbd33b756f
Luka Avramovic <avramoviclu@gmail.com>
2024-01-25 18:19:30 +0100

    Adds Category requests.

---
+++ app/Http/Requests/Api/v1/Category/UpdateCategoryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Api\v1\Category;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|ulid',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string'
+        ];
+    }
+}
