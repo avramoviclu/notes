27a374c673e7c6ea3a489d6c5ae92ad126c25634
Luka Avramovic <avramoviclu@gmail.com>
2024-02-26 22:59:53 +0100

    Implements exception-handling.

--- app/Http/Controllers/Api/v1/NoteController.php
+++ app/Http/Controllers/Api/v1/NoteController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\Api\v1\Note\UpdateNoteRequest;
 use App\Http\Resources\Api\v1\Note\NoteCollection;
 use App\Services\v1\NoteService;
 use App\Http\Resources\Api\v1\Note\NoteResource;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class NoteController extends Controller
 {
@@ -18,38 +21,86 @@ class NoteController extends Controller
         $this->noteService = $noteService;
     }
 
-    public function all(): NoteCollection
+    public function all(): NoteCollection|JsonResponse
     {
-        $data = $this->noteService->all();
+        try {
+            $data = $this->noteService->all();
 
-        return new NoteCollection($data);
+            return new NoteCollection($data);
+
+        } catch(Exception $e) {
+
+            return response()->json(['message' => 'Something went wrong'], $e->getCode());
+        }
     }
 
-    public function store(StoreNoteRequest $request): NoteResource
+    public function store(StoreNoteRequest $request): NoteResource|JsonResponse
     {
-        $data = $this->noteService->store($request->all());
+        try {
+            DB::beginTransaction();
 
-        return new NoteResource($data);
+            $data = $this->noteService->store($request->all());
+
+            DB::commit();
+
+            return new NoteResource($data);
+
+        } catch(Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json(['message' => 'Something went wrong'], $e->getCode());
+        }
     }
 
-    public function show(string $id): NoteResource
+    public function show(string $id): NoteResource|JsonResponse
     {
-        $data = $this->noteService->show($id);
+        try {
+            $data = $this->noteService->show($id);
 
-        return new NoteResource($data);
+            return new NoteResource($data);
+
+        } catch(Exception $e) {
+
+            return response()->json(['message' => 'Something went wrong'], $e->getCode());
+        }
     }
 
-    public function update(UpdateNoteRequest $request, string $id): NoteResource
+    public function update(UpdateNoteRequest $request, string $id): NoteResource|JsonResponse
     {
-        $data = $this->noteService->update($id, $request->all());
+        try {
+            DB::beginTransaction();
 
-        return new NoteResource($data);
+            $data = $this->noteService->update($id, $request->all());
+
+            DB::commit();
+
+            return new NoteResource($data);
+
+        } catch(Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json(['message' => 'Something went wrong'], $e->getCode());
+        }
     }
 
-    public function delete(string $id): NoteResource
+    public function delete(string $id): NoteResource|JsonResponse
     {
-        $data = $this->noteService->delete($id);
+        try {
+            DB::beginTransaction();
 
-        return new NoteResource($data);
+            $data = $this->noteService->delete($id);
+
+            DB::commit();
+
+            return new NoteResource($data);
+
+        } catch(Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json(['message' => 'Something went wrong'], $e->getCode());
+        }
     }
 }
